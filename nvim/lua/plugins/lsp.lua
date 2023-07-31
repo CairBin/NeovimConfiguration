c4ee3776634f3d16c5a96de27728bf08a22990f8
CairBin <1147627792@qq.com>
2023-07-31 15:23:50 +0800

    Update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -13,6 +13,19 @@ require("mason-lspconfig").setup({
     "lua_ls",
     "rust_analyzer",
     "clangd",
+    "html",
+    "asm_lsp",
+    "pyright",
+    "cmake",
+    "volar",
+    "csharp_ls",
+    "cssls",
+    "bashls",
+    "jsonld",
+    "java_language_server",
+    "tsserver",
+    "eslint",
+    "yamlls",
   },
 })
 
@@ -27,5 +40,57 @@ require("lspconfig").clangd.setup {
 }
 
 require("lspconfig").rust_analyzer.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").html.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").asm_lsp.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").pyright.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").cmake.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").volar.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").csharp_ls.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").cssls.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").bashls.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").jsonld.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").java_language_server.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").tsserver.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").eslint.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").yamlls.setup{
   capabilities = capabilities,
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "cmake","go","make","php","ruby","sql","vue","yaml","c", "lua", "vim", "help","cpp","json","java","bash","python","c_sharp","html","css","javascript","rust","typescript","markdown","markdown_inline","tsx" },
+  ensure_installed = { "cmake","go","make","php","ruby","sql","vue","yaml","c", "lua", "vim","cpp","json","java","bash","python","c_sharp","html","css","javascript","rust","typescript","markdown","markdown_inline","tsx" },
   highlight = {enable=true},
   indent = {enable = true},
 
